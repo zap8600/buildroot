542acd479e65f79afedaeca8f30af789277f63bb
Max Filippov <jcmvbkbc@gmail.com>
2017-06-26 13:02:43 -0700

    package/xrp: make more configurable
    Extract DSP core name, number of cores and LSP pattern into the BR
    configuration.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- package/xrp/xrp.mk
+++ package/xrp/xrp.mk
@@ -14,21 +14,22 @@ XRP_CONFIG-$(BR2_PACKAGE_XRP_XTENSA_NEED_SYSTEM) = XTENSA_SYSTEM=$(BR2_PACKAGE_X
 
 define XRP_BUILD_CMDS
   $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/xrp-linux-native
-  for CORE in 0 1 ; do \
+  for (( CORE=0; CORE < $(BR2_PACKAGE_XRP_DSP_NUM_CORES); ++CORE )) ; do \
     $(MAKE) \
 	$(XRP_CONFIG-y) \
 	PATH=$(BR2_PACKAGE_XRP_XTENSA_TOOLS):$$PATH \
 	-C $(@D)/xrp-dsp \
-	DSP_CORE=visionp6cnn_ao_exls \
-	LSP=MW-MP/P6_$$CORE/xtensa-elf/lib/sim-stack-local && \
+	DSP_CORE=$(BR2_PACKAGE_XRP_DSP_CORE_NAME) \
+	LSP=`printf $(BR2_PACKAGE_XRP_DSP_LSP) $${CORE}` && \
     mv $(@D)/xrp-dsp/xrp-dsp{,$$CORE} ; \
   done
 endef
 
 define XRP_INSTALL_TARGET_CMDS
   $(INSTALL) -D -m 0755 $(@D)/xrp-linux-native/linux-native-test $(TARGET_DIR)/root
-  $(INSTALL) -D -m 0644 $(@D)/xrp-dsp/xrp-dsp0 $(TARGET_DIR)/lib/firmware/xrp0.elf
-  $(INSTALL) -D -m 0644 $(@D)/xrp-dsp/xrp-dsp1 $(TARGET_DIR)/lib/firmware/xrp1.elf
+  for (( CORE=0 ; CORE < $(BR2_PACKAGE_XRP_DSP_NUM_CORES); ++CORE )) ; do \
+    $(INSTALL) -D -m 0644 $(@D)/xrp-dsp/xrp-dsp$${CORE} $(TARGET_DIR)/lib/firmware/xrp$${CORE}.elf ; \
+  done
 endef
 
 XRP_MODULE_SUBDIRS = xrp-kernel

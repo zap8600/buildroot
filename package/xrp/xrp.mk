1276a562821b7860d7c8a0b61637ae38e13f2fdb
Max Filippov <jcmvbkbc@gmail.com>
2017-06-28 13:50:00 -0700

    package/xrp: make userspace/DSP building optional
    Add two configure symbols: BR2_PACKAGE_XRP_USERSPACE controls userspace
    example build, BR2_PACKAGE_XRP_FIRMWARE controls firmware build.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- package/xrp/xrp.mk
+++ package/xrp/xrp.mk
@@ -11,8 +11,11 @@ XRP_LICENSE = ?
 
 XRP_CONFIG-$(BR2_PACKAGE_XRP_XTENSA_NEED_SYSTEM) = XTENSA_SYSTEM=$(BR2_PACKAGE_XRP_XTENSA_SYSTEM)
 
-define XRP_BUILD_CMDS
+define XRP_USERSPACE_BUILD-y
   $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/xrp-linux-native
+endef
+
+define XRP_FIRMWARE_BUILD-y
   for (( CORE=0; CORE < $(BR2_PACKAGE_XRP_DSP_NUM_CORES); ++CORE )) ; do \
     $(MAKE) \
 	$(XRP_CONFIG-y) \
@@ -24,11 +27,24 @@ define XRP_BUILD_CMDS
   done
 endef
 
-define XRP_INSTALL_TARGET_CMDS
+define XRP_BUILD_CMDS
+  $(XRP_USERSPACE_BUILD-$(BR2_PACKAGE_XRP_USERSPACE))
+  $(XRP_FIRMWARE_BUILD-$(BR2_PACKAGE_XRP_FIRMWARE))
+endef
+
+define XRP_USERSPACE_INSTALL-y
   $(INSTALL) -D -m 0755 $(@D)/xrp-linux-native/linux-native-test $(TARGET_DIR)/root
+endef
+
+define XRP_FIRMWARE_INSTALL-y
   for (( CORE=0 ; CORE < $(BR2_PACKAGE_XRP_DSP_NUM_CORES); ++CORE )) ; do \
     $(INSTALL) -D -m 0644 $(@D)/xrp-dsp/xrp-dsp$${CORE} $(TARGET_DIR)/lib/firmware/xrp$${CORE}.elf ; \
   done
+endef
+
+define XRP_INSTALL_TARGET_CMDS
+  $(XRP_USERSPACE_INSTALL-$(BR2_PACKAGE_XRP_USERSPACE))
+  $(XRP_FIRMWARE_INSTALL-$(BR2_PACKAGE_XRP_FIRMWARE))
 endef
 
 XRP_MODULE_SUBDIRS = xrp-kernel

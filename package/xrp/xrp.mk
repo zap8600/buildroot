53c18c3f4ae40a6fe8d0ef7ab3563e2a8e5d1fe6
Max Filippov <jcmvbkbc@gmail.com>
2017-06-28 16:06:51 -0700

    package/xrp: update to the version xrp-20170621
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- package/xrp/xrp.mk
+++ package/xrp/xrp.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-XRP_VERSION = xrp-20170117
+XRP_VERSION = xrp-20170621
 XRP_SITE = $(call github,foss-xtensa,xrp,$(XRP_VERSION))
 XRP_SOURCE = $(XRP_VERSION).tar.gz
-XRP_LICENSE = ?
+XRP_LICENSE = MIT, GPL
 
 XRP_CONFIG-$(BR2_PACKAGE_XRP_XTENSA_NEED_SYSTEM) = XTENSA_SYSTEM=$(BR2_PACKAGE_XRP_XTENSA_SYSTEM)
 
